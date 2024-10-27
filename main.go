a82266809519bb2f3e1771c539888523076cb9de
Léandro DA SILVA <leothefrench@gmail.com>
2024-10-27 11:33:20 +0100

    installation de fiber, air & initialisation du fichier air.toml'

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-	fmt.Println("Hello, Léo")
+	fmt.Println(("Hello, Léo bien sûr"))
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.ctx) error {
+		return c.Status(200).JSON(fiber.Map{"msg": "Hello Léo"})
+	})
+
+	log.Fatal(app.Listen(":4000"))
 }
